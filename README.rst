9958ef88ae403f43c826f93301061fa5d196c7ae
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2020-12-07 12:50:05 +0100

    Moves latlon module to ./latlon creating latlon package.

---
+++ README.rst
@@ -0,0 +1,29 @@
+Latlon/latlonUTM package
+========================
+
+Synopsis
+--------
+
+Some utilities to work with coordinates in latitude and longitude.
+
+This module provides two packages, namely latlon and latlonUTM.
+
+The latlonUTM package provides a way to translate a latitude longitude
+coordinate to a Universal Transfer Mercator (UTM) coordinate, see also
+Wikipedia https://en.wikipedia.org/wiki/Universal_Transverse_Mercator_coordinate_system.
+
+The latlon package introduces a class that represents a position in
+latitude and longitude. Methods are provided to compute the distance
+between points, the direction from one point to another, translate
+points etc.
+
+Distance calculations are based on the latlonUTM package.
+
+Installation
+------------
+
+The installation of the package requires compiling C-code for the
+latlonUTM module. Therefore, a correct installation of these packages
+may require the installation of gcc and the development headers of python.
+
+
--- setup.py
+++ setup.py
@@ -1,14 +1,66 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
-setup (name = 'latlon',
-       version = '0.3',
-       description = 'A bundle of classes useful when working with lat/lon coordinates',
-       author = 'Lucas Merckelbach',
-       author_email = 'lmm@noc.soton.ac.uk',
-       url = '',
-       py_modules=['latlon'],
-       packages = [],
-       long_description ='A bundle of classes useful when working with lat/lon coordinates')
+import os
+from setuptools import find_packages, setup, Extension
 
+
+about = {'name':"latlon"}
+
+
+with open(f"{about['name']}/_version.py", encoding="utf-8") as f:
+    exec(f.read(), about)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), encoding='utf8') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as fobj:
+    install_requires = [line.strip() for line in fobj]
+
+
+latlonUTM_ext = Extension('_latlonUTM',
+                          define_macros = [('MAJOR_VERSION', '0'),
+                                           ('MINOR_VERSION', '1')],
+                          include_dirs = ['./include'],
+                          libraries = [],
+                          library_dirs = ['.'],
+                          sources = ['extension/_latlonUTMmodule.c',\
+                                     'extension/latlon.c',\
+                                     'extension/latlong_utm.c'])
+    
+setup(
+    name=about['name'],
+    version=about['__version__'],
+    description='Lat/lon and UTM utilities',
+    long_description=long_description,
+    url='https://github.com/',
+    author='Lucas Merckelbach',
+    author_email='lucas.merckelbach@hzg.de',
+    license='GPLv3',
+    packages=find_packages(where='.', exclude=['tests', 'docs', 'examples']),
+    ext_modules = [latlonUTM_ext],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GPLv3 License',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords=['ocean gliders', 'glider flight', 'oceanography'],
+    install_requires=install_requires,
+    include_package_data=True,
+    #scripts=[''],
+    #py_modules=[''],
+
+)
+# how to provide scripts etct
+#      py_modules = ['fastachar'],
+#      entry_points = {'console_scripts':[],#['fastachar_gui = fastachar_gui:main
+#                      'gui_scripts':['fastachar = fastachar:main']
+#                      },
 
 
